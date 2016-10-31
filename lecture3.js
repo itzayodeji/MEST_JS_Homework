44d74eb7e0fc3688b787db7afb349e49c809ad4b
itzayodeji <ayodejioyinloye@meltwater.org>
2016-10-31 22:12:49 +0000

    Last modified commit

--- lecture3.js
+++ lecture3.js
@@ -17,13 +17,21 @@
  *  var obj = {
  *      func: func // what is the second func here?
  *  };
+
+ 	ANSWER: This represents the func functioni that calls itself
  *
  *  obj.func(); //what is the output? try to explain why.
+ 		ANSWER: This is an object with yields an empty object because it is bound to an empty object
  *
  *  obj.func.bind(window)(); // what is the output? why?
+ 	ANSWER: This adds the func function into the windows function cache because it is now bound to
+ 			the window Object.
  *
  *  obj.func.bind(this)();  // what is the output? why?
  *
+ 	ANSWER: This adds the func function into the windows function cache because it is now bound to
+ 			the window Object.
+
  *  var people = {
  *      name: 'ashwin',
  *      age: 40
@@ -31,6 +39,8 @@
  *
  *  obj.func.bind(people)(); //what is the output? why?
  */
+ 	Object{name: 'ashwin',age: 40};
+ 	People is an object the gets bound to itself by the func
 
 /* 2. write a function to convert first letter of every word in a string
  *     to upper case.
@@ -40,11 +50,27 @@
  *        output2 = 'Today is Sunday';
  */
 
+ 	function capitalizeWords(words){
+ 		words = str.split(" ")
+		newWords = []
+		words.forEach(function(word){
+			slicedWord = word.slice(1)
+			newWords.push(word[0].toUpperCase() + slicedWord);
+			return newWords
+		});
+
+		str2 = newWords.join(" ");
+		return str2;
+ 	}
+		
+		
+
 /*
  *  3. Write reg expresion for email validation(This is not an easy
  *  question, please make sure you do your research on this).
  *
  */
+
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
  
 /*
